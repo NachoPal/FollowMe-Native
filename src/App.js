c7f0ff22af4ffc38e8ede3694d1ee39ddf7cdebc
Nacho <ignacio.palacios.santos@gmail.com>
2017-01-18 23:00:50 +0100

    Try

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ export default class App extends Component {
       <Provider store={store}>
         <View>
           <Text>Hello!</Text>
+          <Text>World!</Text>
         </View>
       </Provider>
     );
